98f9955d379d3ffd1a829721900a7c183bf0ba44
lmaosoggypancakes <macerajosef@gmail.com>
2023-05-14 06:43:00 -0700

    fix: animation pre-rendered on ssr

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,23 +3,19 @@
     <div class="lg:ml-0 ml-8">
       <h1
         class="lg:text-7xl text-5xl mt-[10%] float-left"
-        v-motion
-        :initial="{
-          y: 100,
-          opacity: 0,
+        v-motion="{
+          initial: { y: 100, opacity: 0 },
+          enter: { y: 0, opacity: 1, transition: { delay: 250 } },
         }"
-        :enter="{ y: 0, opacity: 1, transition: { delay: 250 } }"
       >
         Hello, I'm <span class="text-secondary">Josef Macera</span>.
       </h1>
       <p
         class="float-left mt-8 lg:w-3/4 text-2xl"
-        v-motion
-        :initial="{
-          y: 100,
-          opacity: 0,
+        v-motion="{
+          initial: { y: 100, opacity: 0 },
+          enter: { y: 0, opacity: 1, transition: { delay: 500 } },
         }"
-        :enter="{ y: 0, opacity: 1, transition: { delay: 500 } }"
       >
         I'm a full-stack web developer based in Reno, Nevada. I'm an open source
         enthusiast and enjoy anything to do with computers.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig(<any>{
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@vueuse/motion/nuxt",
-    "nuxt-icon",
-    "@vueuse/nuxt",
-  ],
-  motions: {
-    directives: {
-      "pop-bottom": {
-        initial: {
-          scale: 0,
-          opacity: 0,
-          y: 100,
-        },
-        visible: {
-          scale: 1,
-          opacity: 1,
-          y: 0,
-        },
-      },
-    },
-  },
+export default defineNuxtConfig({
+  ssr: true,
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/motion/nuxt", "nuxt-icon"],
+  css: ["@/assets/global.css"],
 });
