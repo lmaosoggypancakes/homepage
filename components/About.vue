41291f9616b92154e025059aa1adc1c9a2986f07
lmaosoggypancakes <macerajosef@gmail.com>
2023-05-20 23:04:49 -0700

    feat: about me section

--- components/About.vue
+++ components/About.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="h-[80vh] grid md:grid-cols-2">
-    <div class="md:p-0 p-4">
+    <div
+      class="md:p-0 p-4 text-xl"
+      v-motion="{
+        initial: { x: -200, opacity: 0 },
+        visible: {
+          x: 0,
+          opacity: 1,
+        },
+      }"
+    >
       <h1 class="text-5xl font-extrabold text-secondary mb-4">About Me</h1>
-      I've been programming
+      I'm a self-taught developer who loves nothing more than learning new
+      things and passing on what I know to others. I have a passion for app
+      development and building AI projects, as well as supporting the
+      open-source community. In my spare time, I enjoy building computers,
+      ricing Linux machines, and praticing martial arts.
+      <br />
+      <br />
+      I'll be attending Carnegie Mellon Univerisity in the fall, where I plan to
+      study electrical and computer engineering. Feel free to reach out! :)
     </div>
     <div>
       <ul
@@ -11,7 +28,7 @@
         <li v-for="(skill, index) in skills">
           <Icon
             :name="skill.icon"
-            class="h-12 w-12 hover:pb-2 hover:text-secondary transition-all"
+            class="h-12 w-12 hover:pb-2 text-primary transition-all"
             v-motion="{
               initial: { y: 100, opacity: 0 },
               visible: {
