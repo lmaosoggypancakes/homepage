0be473ecac7f98d5427385c116ea5cecb8142b55
lmaosoggypancakes <macerajosef@gmail.com>
2023-05-24 00:21:56 -0700

    feat: switch to SSG for faster load

--- server/api/repos.ts
+++
@@ -1,8 +0,0 @@
-import { GitHubRepo } from "~/types";
-import axios from "axios";
-export default defineEventHandler(async (event): Promise<GitHubRepo[]> => {
-  const response = await axios.get(
-    "https://gh-pinned-repos.egoist.dev/?username=lmaosoggypancakes"
-  );
-  return <GitHubRepo[]>response.data;
-});
